11ed497dd33d73f0b98928effe7ef771311d1cc0
kvark <info@kvark.si>
2024-04-16 14:12:19 +0200

    NextLink7

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import { default as NextLink } from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
